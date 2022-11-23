987baf5c51114a97ffee3f51bc084d439f5913f6
Michalis <57771939+MichelStath@users.noreply.github.com>
2022-11-23 10:15:42 +0200

    Dial Ready TODO Make call

--- app/src/main/java/com/example/androidlauncherproject/DialActivity.java
+++ app/src/main/java/com/example/androidlauncherproject/DialActivity.java
@@ -1,5 +1,9 @@
 package com.example.androidlauncherproject;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,18 +11,29 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.xml.sax.helpers.LocatorImpl;
 
 public class DialActivity extends AppCompatActivity {
 
     TextView phoneNumber;
+    static int PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
         phoneNumber = findViewById(R.id.phoneInp);
+
+        if(ContextCompat.checkSelfPermission(DialActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(DialActivity.this,new String[]{Manifest.permission.CALL_PHONE},PERMISSION_CODE);
+        }
+
     }
 
     public void clicktest(View view) {
@@ -48,5 +63,19 @@ public class DialActivity extends AppCompatActivity {
     }
 
     public void makeCAll(View view) {
+        Log.i("Dial Activity", "Call BTN Pressed");
+        String phoneNum = phoneNumber.getText().toString().trim();
+        Intent i = new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:" + phoneNum));
+        startActivity(i);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != PERMISSION_CODE || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
     }
 }
