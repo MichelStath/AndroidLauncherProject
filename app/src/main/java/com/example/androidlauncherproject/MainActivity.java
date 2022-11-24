67952f36f9d347b8cc4890aa4c6bcc617e8780e5
Michalis <57771939+MichelStath@users.noreply.github.com>
2022-11-24 16:11:04 +0200

    add menu

--- app/src/main/java/com/example/androidlauncherproject/MainActivity.java
+++ app/src/main/java/com/example/androidlauncherproject/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.androidlauncherproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -36,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 
     public void test(){
